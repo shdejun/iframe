aed7931091585ad64d218a06d98452812e375203
lyj <lyj@chehaha.net>
2020-01-17 14:46:33 +0800

    button change

--- examples/App.vue
+++ examples/App.vue
@@ -1,7 +1,6 @@
 <template>
   <div id="app">
-    
-    <router-view class="center-views"/>
+    <router-view />
   </div>
 </template>
 
@@ -15,21 +14,11 @@ export default {
 
 <style lang="scss">
 @import './assets/css/reset';
+@import './assets/css/common';
 #app {
   width: 100%;
   height: 100%;
   background-color: #f7f8fa;
-  .nav{
-    width: 200px;
-    height: calc(100% - 80px);
-    display: inline-block;
-    overflow-y: auto;
-    vertical-align: top;
-  }
-  .center-views{
-    display: inline-block;
-    
-    width: calc(100% - 243px)
-  }
+  padding: 20px 10px;
 }
 </style>
--- examples/components/button.vue
+++ examples/components/button.vue
@@ -1,11 +1,22 @@
 <template>
   <div>
-    <yj-button>默认按钮</yj-button>
-  <yj-button type="primary">默认按钮</yj-button>
-  <yj-button type="info">信息按钮</yj-button>
-  <yj-button type="danger">危险按钮</yj-button>
-  <yj-button type="warning">警告按钮</yj-button>
-  <yj-button plain type="warning">警告按钮</yj-button>
+    <div class="demon-item">
+      <h2 class="demon-item-title">按钮类型</h2>
+      <yj-button>默认按钮</yj-button>
+      <yj-button type="primary">默认按钮</yj-button>
+      <yj-button type="info">信息按钮</yj-button>
+      <yj-button type="danger">危险按钮</yj-button>
+      <yj-button type="warning">警告按钮</yj-button>
+      <yj-button plain type="warning">警告按钮</yj-button>
+    </div>
+    <div class="demon-item">
+      <h2 class="demon-item-title">朴素按钮</h2>
+      <yj-button plain>默认按钮</yj-button>
+      <yj-button type="primary" plain>默认按钮</yj-button>
+      <yj-button type="info" plain>信息按钮</yj-button>
+      <yj-button type="danger" plain>危险按钮</yj-button>
+      <yj-button plain type="warning">警告按钮</yj-button>
+    </div>
   </div>
 </template>
 
